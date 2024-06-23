2877d7dc550baea78306ec79634c3fe6bde1a955
Manu Emmanuel <manuemmanuel228@gmail.com>
2024-06-23 16:06:11 +0530

    fonts

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ def generate_screenshot:
     text_color = (255, 255, 255)
     username_color = (63, 116, 98)
     symbol_color = (55, 59, 105)
-    
+    font_path = "fonts/UbuntuMono-R.ttf"
+    bold_font_path = "fonts/UbuntuMono-B.ttf"
+    font_size = 20
